80da5c96a067c8e645602d13003bfec63fd93446
Morten010 <97233551+Morten010@users.noreply.github.com>
2022-10-20 22:39:55 +0200

    Small css fixes

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 let btn = document.querySelectorAll('.accordian-btn');
 
+//goes through a loop to put an Eventlistener on each accordion which toggles the class open
 for(let i = 0; i < btn.length; i++){
     btn[i].addEventListener('click', (e) => {
         let grandparent = btn[i].parentElement.parentElement;
-        // console.log(grandparent);
         grandparent.classList.toggle('open')
     });
 }
