549ba98fe33852cdebd2dea17847bf0752b95802
Morten010 <97233551+Morten010@users.noreply.github.com>
2022-10-21 23:41:44 +0200

    burger menu

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
 let btn = document.querySelectorAll('.accordian-btn');
-
+let burgerBtn = document.querySelectorAll('.burger-btn')
 //goes through a loop to put an Eventlistener on each accordion which toggles the class open
 for(let i = 0; i < btn.length; i++){
     btn[i].addEventListener('click', (e) => {
         let grandparent = btn[i].parentElement.parentElement;
         grandparent.classList.toggle('open')
     });
+}
+
+for(let i = 0; i < burgerBtn.length; i++){
+    console.log(i)
+    burgerBtn[i].addEventListener('click', (e) =>{
+        let nav = document.querySelector('.nav')
+        nav.classList.toggle('open')
+    })
 }
